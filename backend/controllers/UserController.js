1d8aacc1b68908456e20a84b5f9d5263e71d2d81
AlexMoreni <alex.moreni47@gmail.com>
2023-12-13 13:28:04 -0300

    Verifying user by token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/Users");
+//Helpers
+const getToken = require("../helpers/get-token");
 const createUserToken = require("../helpers/create-user-token");
 
 module.exports = class UserController {
@@ -88,5 +92,23 @@ module.exports = class UserController {
     }
 
     await createUserToken(user, req, res);
+  }
+
+  static async checkUser(req, res) {
+    let currentUser;
+
+    if (req.headers.authorization) {
+      const token = getToken(req);
+
+      const decoded = jwt.verify(token, "secret");
+
+      currentUser = await User.findById(decoded.id);
+
+      currentUser.password = undefined;
+    } else {
+      currentUser = null;
+    }
+
+    res.status(200).send(currentUser);
   }
 };
